f367c4ae6d5e306dc3dcd4868c84c25a82271ea1
th3cr00k3dm4n <73489161+th3cr00k3dm4n@users.noreply.github.com>
2023-01-25 06:29:09 +0530

    commit

--- .scripts/batstat.sh
+++ .scripts/batstat.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
+while true
+do
 
-# Set the high and low battery thresholds (in percent)
-high_battery_threshold=80
-low_battery_threshold=25
+STATE=$(acpi -a | grep "on-line" > /dev/null 2>&1 && echo "Y" || echo "N")
+LEVEL=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+if [[ "$STATE" == "Y" ]]; then
 
-# Set the check interval (in seconds)
-check_interval=60
-
-while true; do
-  # Get the current battery level and status
-  battery_level=$(acpi -b | awk '{print $4}' | tr -d ',')
-
-  # Check if the battery level is above the high battery threshold
-  if [ "$battery_level" -ge "$high_battery_threshold" ]; then
-    # Send a notification
-    notify-send "High Battery: $battery_level%"
-
-  # Check if the battery level is below the low battery threshold
-  elif [ "$battery_level" -le "$low_battery_threshold" ]; then
-    # Send a notification
-    notify-send "Low Battery: $battery_level%"
-  fi
-
-  # Sleep for the specified interval before checking again
-  sleep "$check_interval"
+	if  [  "$LEVEL" -gt 80 ]; then
+			notify-send "  Battery Going to explod "
+	fi
+else 
+	if  [  "$LEVEL" -lt 25 ]; then
+			notify-send -u critical  "Running out of fuel :( "
+	fi
+fi
+sleep 50
 done
