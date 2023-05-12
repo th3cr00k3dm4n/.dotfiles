8b9cfb47f89a6be652c0186c14ff8c27ded7f038
th3cr00k3dm4n <73489161+th3cr00k3dm4n@users.noreply.github.com>
2023-05-12 19:14:48 +0530

    fix

--- .scripts/wal.sh
+++ .scripts/wal.sh
@@ -3,9 +3,12 @@ IMG=$(find ~/wallpaper -type f \( -name "*.png" -o -name "*.jpg" \)  | shuf -n1)
 sleep 0.3
 swaybg -i "$IMG" -m fill >/dev/null 2>&1 &  
 sleep 0.3
-wal -i "$IMG" >/dev/null 2>&1 && pkill waybar  && waybar >/dev/null 2>&1 & disown &&
-/home/cr00k3d/.scripts/alacritty-wal.sh
+rm -rf .cache/wall/* 
+sleep 0.3
+wal -i "$IMG" >/dev/null 2>&1 
 pywalfox update
+/home/cr00k3d/.scripts/alacritty-wal.sh
+pkill waybar  && waybar >/dev/null 2>&1 & disown &&
 sleep 0.5 
 exit
 
